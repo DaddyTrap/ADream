0893e2306794d90058005c5156134d7bda04882f
DaddyTrap <914670787@qq.com>
2018-01-04 16:52:31 +0800

    :lipstick: :sparkles: 更改背景图片实现方法，加入音乐播放UI

--- app/src/main/java/io/github/daddytrap/adream/activity/MusicActivity.java
+++ app/src/main/java/io/github/daddytrap/adream/activity/MusicActivity.java
@@ -1,15 +1,34 @@
 package io.github.daddytrap.adream.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import io.github.daddytrap.adream.ADApplication;
 import io.github.daddytrap.adream.R;
 
 public class MusicActivity extends AppCompatActivity {
 
+    private TextView musicTitle;
+    private TextView musicLyric;
+
+    private ADApplication app;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_music);
+        super.setContentView(R.layout.activity_music);
+
+        app = ADApplication.getInstance();
+
+        setViews();
+    }
+
+    private void setViews() {
+        musicTitle = (TextView) findViewById(R.id.activity_music_title);
+        musicLyric = (TextView) findViewById(R.id.activity_music_lyric);
+
+        musicTitle.setTypeface(app.KAI_TI_FONT);
+        musicLyric.setTypeface(app.KAI_TI_FONT);
     }
 }
