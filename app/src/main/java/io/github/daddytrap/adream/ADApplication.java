80d2e07dfa728175cd4da78f1b14a38f7d005391
Ken-reddevil <741875304@qq.com>
2018-01-06 20:04:39 +0800

    add base64 to bitmap function

--- app/src/main/java/io/github/daddytrap/adream/ADApplication.java
+++ app/src/main/java/io/github/daddytrap/adream/ADApplication.java
@@ -1,11 +1,16 @@
 package io.github.daddytrap.adream;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
 import android.os.Environment;
+import android.util.Base64;
 import android.util.Log;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by DaddyTrapC on 2018/1/3.
@@ -15,6 +20,8 @@ public class ADApplication extends Application {
     private static ADApplication _ADApplication;
 
     public Typeface KAI_TI_FONT;
+
+    private Map<String, Bitmap> bitmapCache = null;
 
     @Override
     public void onCreate() {
@@ -32,9 +39,19 @@ public class ADApplication extends Application {
                 Log.e(ADApplication.class.getName(), "Create dir failed");
             }
         }
+
+        bitmapCache = new HashMap<>();
     }
 
     public static ADApplication getInstance() {
         return _ADApplication;
+    }
+
+    public Bitmap getBitmap(String strBase64) {
+        if (bitmapCache.containsKey(strBase64)) return bitmapCache.get(strBase64);
+        byte[] bytes = Base64.decode(strBase64, Base64.DEFAULT);
+        Bitmap bm = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        bitmapCache.put(strBase64, bm);
+        return bm;
     }
 }
